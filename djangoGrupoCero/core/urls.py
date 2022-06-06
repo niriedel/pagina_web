913390b9c0bd7680207c18be210d6e2bf2e18807
nrdj <nriedeldj@gmail.com>
2022-06-06 11:45:40 -0400

    arreglado artistas y categorias

--- djangoGrupoCero/core/urls.py
+++ djangoGrupoCero/core/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import index, login, register
+from .views import index, login, register, api, artista_dad, artista_imp, artista_med, ca_dad, ca_imp, ca_med
 
 urlpatterns = [
     path('', index, name="index"),
+    #carpeta acceso
     path('acceso/login/', login, name="login"),
     path('acceso/register/', register, name="register"),
+    #carpeta api
+    path('api/', api, name="api"),
+    #carpeta artistas
+    path('artistas/art_dad/', artista_dad, name="art_dad"),
+    path('artistas/art_imp/', artista_imp, name="art_imp"),
+    path('artistas/art_med/', artista_med, name="art_med"),
+    #carpeta categorias
+    path('categorias/ca_dad/', ca_dad, name="ca_dad"),
+    path('categorias/ca_imp/', ca_imp, name="ca_imp"),
+    path('categorias/ca_med/', ca_med, name="ca_med"),
 
 ]
--- djangoGrupoCero/core/views.py
+++ djangoGrupoCero/core/views.py
@@ -3,14 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-
     return render(request, 'core/index.html')
 
+#carpeta acceso
 def login(request):
-
     return render(request, 'core/acceso/login/index.html')
 
 def register(request):
-
     return render(request, 'core/acceso/register/index.html')
 
+# def aceptarRechazarProducto aqui
+
+#carpeta api
+def api(request):
+    return render(request, 'core/api/index.html')
+
+#carpeta artistas
+def artista_dad(request):
+    return render(request, 'core/artistas/art_dad/index.html')
+
+def artista_imp(request):
+    return render(request, 'core/artistas/art_imp/index.html')
+
+def artista_med(request):
+    return render(request, 'core/artistas/art_med/index.html')
+
+#carpeta categorias
+
+def ca_dad(request):
+    return render(request, 'core/categorias/ca_dad/index.html')
+
+def ca_imp(request):
+    return render(request, 'core/categorias/ca_imp/index.html')
+
+def ca_med(request):
+    return render(request, 'core/categorias/ca_med/index.html')
