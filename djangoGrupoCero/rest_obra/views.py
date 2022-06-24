63fa9a3b962d46a00f4c957872ee2e06563ab932
niRiedel <ni.riedel@duocuc.cl>
2022-06-23 22:19:14 -0400

    fixear bug api rest get

--- djangoGrupoCero/rest_obra/views.py
+++ djangoGrupoCero/rest_obra/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
@@ -12,7 +12,8 @@ from rest_framework.permissions import IsAuthenticated
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
-@permission_classes((IsAuthenticated,))
+@authentication_classes([]) # Add this
+@permission_classes([]) # Maybe add this too
 def lista_obras(request):
     if request.method == 'GET':
         lista = Obra.objects.all()
@@ -28,7 +29,7 @@ def lista_obras(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes((IsAuthenticated,))
+@permission_classes([]) # Maybe add this too
 def detalle_obra(request,id):
     try:
         obra = Obra.objects.get(patente=id)
