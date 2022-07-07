5fd02748645bfbaecfb138698f768cc37430a266
niRiedel <ni.riedel@duocuc.cl>
2022-07-07 19:00:26 -0400

    registro y login de usuario base de datos

--- djangoGrupoCero/core/admin.py
+++ djangoGrupoCero/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Obra
+from .models import Obra, Usuario
 
 # Register your models here.
 admin.site.register(Obra)
+admin.site.register(Usuario)
